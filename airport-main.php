479299d0f682e0d9f31b9738b9f0495f1fe5d0e7
Alectmc <alectmc@gmail.com>
2024-04-16 22:35:38 -0500

    Added Main Webpage To Repo

---
+++ airport-main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Airport Manager</title>
+	</head>
+
+	<body>
+		<h1>WELCOME TO THE AIRPORT MANAGEMENT SYSTEM</h1>
+
+		<br>
+
+		<h3>Please Select An Option</h3>
+		<ul id="choices" style="list-style-type: none;">
+			<li class="listChoices">
+				<a href="./airport-airlines.php">Airlines ~Under Construction~</a>
+			</li>
+			<li class="listChoices">
+				<a href="./airport-flights.php">Flights ~Unavailable~</a>
+			</li>
+			<li class="listChoices">
+				<a href="./airport-staff.php">Staff ~Unavailable~</a>
+			</li>
+			<li class="listChoices">
+				<a href="./airport-passengers.php">Passengers ~Unavailable~</a>
+			</li>
+		</ul>
+	</body>
+</html>
