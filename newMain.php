6e3060922f0b2f32a5cf114301020374c194d458
Alectmc <alectmc@gmail.com>
2024-04-22 16:36:23 -0500

    Began Work on Homepage

---
+++ newMain.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>New Homepage</title>
+		<link href="./styles.css" type="text/css" rel="stylesheet">
+	</head>
+
+	<body>
+		<img src="https://experiencecle.com/wp-content/uploads/2020/06/bna-vert-lockup-rgb.png" style="width: 381px; height: 303px;">
+		<h1 style="font-family: Copperplate, fantasy; text-align: center;">Welcome to BNA</h1>
+
+		<br>
+
+		<ul id="navBar">
+			<li class="topBar" id="active">
+				<a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">Home</a>
+			</li>
+			<li class="topBar">
+				<a href="./newFlights.php">Flights</a>
+			</li>
+			<li class="topBar">
+				<a href="./airport-admin.php">Administrator Login</a>
+			</li>
+		</ul>
+
+	</body>
+</html>
