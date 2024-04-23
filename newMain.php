44ed58947fcd8a6c5432bc7aa3ea215078f142de
mattintn3 <mattintn3@gmail.com>
2024-04-22 23:33:06 -0500

    Update newMain.php

--- newMain.php
+++ newMain.php
@@ -23,5 +23,68 @@
 			</li>
 		</ul>
 
+		<h2>Enter Your Destination</h2>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" onclick="return validateFlightNum()">
+			Destination: <input type="text" name="destination">
+			<input type="submit">
+		</form>
+
+		<span id="errorMessage" style="color: red;"></span>
+		<?php
+			require 'connectDatabase.php';
+
+			if($_SERVER['REQUEST_METHOD'] == "POST") {
+				if(empty($_POST['destination'])) {
+					echo "An error has occurred.";
+				} else {
+					$destination = $_POST['destination'];
+
+					echo "<script>console.log('Connecting to database... ')</script>";
+					$conn = connectDatabase();
+
+					if ($conn->connect_error) {
+						echo "Connection failed: " . $conn->connect_error . "<br>";
+						die();
+					}
+
+					$sql1 = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'flights'";
+					$sql2 = "SELECT * FROM flights WHERE Destination = ? AND Origin = 'Nashville'";
+
+					$columns = mysqli_query($conn, $sql1);
+
+					$stmt = $conn->prepare($sql2);
+					$stmt->bind_param("s", $destination);  // Change to 's' if $destination is a string
+					$stmt->execute();
+					$result = $stmt->get_result();
+
+					if ($result->num_rows == 0) {
+						echo "No flights found.";
+						die();
+					}
+
+					echo "<table border='5'>";
+					echo "<tr>";
+					while($row = mysqli_fetch_assoc($columns)) {
+						foreach($row as $value) {
+							echo "<th>" . $value . "</th>";
+						}
+					}
+
+					echo "</tr>";
+
+					while($row = mysqli_fetch_assoc($result)) {
+						echo "<tr>";
+						foreach($row as $value) {
+							echo "<td>" . $value . "</td>";
+						}
+						echo "</tr>";
+					}
+
+					echo "</table><br>";
+
+					$conn->close();
+				}
+			}
+		?>
 	</body>
 </html>
