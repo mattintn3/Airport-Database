9c6b9a29bd448572ac8b3131dac9bebc5e243414
Alectmc <alectmc@gmail.com>
2024-04-22 20:20:43 -0500

    Keeping Everything Up-To-Date

--- newMain.php
+++ newMain.php
@@ -18,6 +18,9 @@
             <li class="topBar">
                 <a href="./newFlights.php">Flights</a>
             </li>
+			<li class="topBar">
+				<a href="./book-flight.php">Book A Flight</a>
+			</li>
             <li class="topBar">
                 <a href="./airport-admin.php">Administrator Login</a>
             </li>
